a6bfb4ffbb9e125f610115b0683edc8060fae2eb
Tom O'Connell <toconnel@caltech.edu>
2020-06-02 23:48:38 -0700

    Progress, saving outputs, debugger shell at end

--- caltech_name_checker.py
+++ caltech_name_checker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 from pprint import pprint
 # Not sure if this is builtin or just something we have from installing
 # mechanize
@@ -7,9 +8,20 @@ import urllib
 
 import pandas as pd
 import mechanize
+from tqdm import tqdm
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', action='store',
+        help='Only process the first N rows.'
+    )
+    args = parser.parse_args()
+    if args.n:
+        n = int(args.n)
+    else:
+        n = None
+
     petition_excel = 'sign_list.xlsx'
     pdf = pd.read_excel(petition_excel)
 
@@ -19,8 +31,15 @@ def main():
     br = mechanize.Browser()
     form_idx = 1
 
+    if n:
+        form_data_rows = form_data_rows[:n]
+        n_rows = len(form_data_rows)
+        assert n_rows == n
+
+    good_indices = []
+    bad_indices = []
     possible_noncaltech = []
-    for i, form_data_row in enumerate(form_data_rows):
+    for i, form_data_row in tqdm(enumerate(form_data_rows), total=n_rows):
         # Could also get part after and include an request to the Advanced
         # Search form if you wanted.
         name = form_data_row.split('(')[0].strip()
@@ -35,7 +54,9 @@ def main():
             # We don't actually care what the response is, just that we don't
             # get a 404. Could probably make this more efficient.
             response = br.submit()
+            good_indices.append(i)
         except urllib.error.HTTPError as e:
+            bad_indices.append(i)
             possible_noncaltech.append((i, form_data_row, name))
 
     if len(possible_noncaltech) > 0:
@@ -43,6 +64,14 @@ def main():
         pprint(possible_noncaltech)
     else:
         print('All good')
+
+    good_df = pdf.iloc[good_indices]
+    good_df.to_csv('good.csv')
+
+    bad_df = pdf.iloc[bad_indices]
+    bad_df.to_csv('bad.csv')
+
+    import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__':
